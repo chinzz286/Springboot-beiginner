06db1c07b630fbd56b050fbf5fd7ac2cf74d9cad
Aluwala Chinmayee <achinmayee999@gmail.com>
2026-07-27 22:42:02 +0530

    Initialised the springboot project

---
+++ src/main/java/com/chinmayee/Springboot_beiginner/SpringbootBeiginnerApplication.java
@@ -0,0 +1,13 @@
+package com.chinmayee.Springboot_beiginner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBeiginnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBeiginnerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chinmayee/Springboot_beiginner/SpringbootBeiginnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chinmayee.Springboot_beiginner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBeiginnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
