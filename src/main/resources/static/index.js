4fab3d9f0b89ccb4dd08982a6e562c7ab6255d42
Aluwala Chinmayee <achinmayee999@gmail.com>
2026-07-28 23:09:01 +0530

    Created random user card

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -22,3 +22,20 @@ function toggleUser(){
     userName.innerHTML = users[id].name;
     userGender.innerHTML = users[id].gender;
 }
+function randomUser(){
+    fetch('https://randomuser.me/api')
+    .then(function(res) {
+        return res.json();
+    })
+    .then(function(data) {
+        var userImage = document.getElementById("user-image");
+        var userName = document.getElementById("user-name");
+        var userGender = document.getElementById("user-gender");
+        userImage.src = data.results[0].picture.large;
+        userName.innerHTML = data.results[0].name.first + " " + data.results[0].name.last;
+        userGender.innerHTML = data.results[0].gender;   
+    })
+    .error(function(err) {
+            console.log(err);
+        })
+}
